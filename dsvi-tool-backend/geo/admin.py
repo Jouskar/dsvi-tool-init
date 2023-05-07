168a06d5bebdc3f2ab2fe78adbcf88f08b53f68b
TOLGAHAN OZBEK <ceng_tolga@hotmail.com>
2023-05-07 03:08:40 +0300

    Add raster to admin

--- dsvi-tool-backend/geo/admin.py
+++ dsvi-tool-backend/geo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django import forms
 from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.gdal import GDALRaster
 from geo.models import VectorModel, RasterModel, PointModel
 import json
 
@@ -27,6 +28,24 @@ class VectorForm(forms.ModelForm):
         return instance
 
 
+class RasterForm(forms.ModelForm):
+    raster_file = forms.FileField()
+
+    class Meta:
+        model = RasterModel
+        fields = ('name', 'raster_file', 'country', 'region', 'styling', 'editor', 'status')
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        raster_file = self.cleaned_data.get('raster_file')
+        if raster_file:
+            raster = GDALRaster(raster_file)
+            instance.dataa = json.dumps(raster)
+        if commit:
+            instance.save()
+        return instance
+
+
 @admin.register(VectorModel)
 class VectorAdmin(admin.ModelAdmin):
     form = VectorForm
@@ -34,7 +53,7 @@ class VectorAdmin(admin.ModelAdmin):
 
 @admin.register(RasterModel)
 class RasterAdmin(admin.ModelAdmin):
-    pass
+    form = RasterForm
 
 
 @admin.register(PointModel)
